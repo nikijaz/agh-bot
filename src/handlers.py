b29794966c4c906efe8bc74e787d54a26e5ee2ba
Mikita Sirosh <mikita.sirosh@gmail.com>
2025-08-02 01:17:14 +0200

    Improve code maintainability and readability
    - Replace implicit IIFEs with decorators for clearer flow
    - Encapsulate Telegram captcha callback data in a dedicated class

--- src/handlers.py
+++ src/handlers.py
@@ -6,6 +6,7 @@ from i18n import t
 
 from src import BOT, DP
 from src.features import anecdote, captcha
+from src.features.captcha import CaptchaCallback
 
 
 @DP.message(~F.from_user.is_bot & F.chat.type != ChatType.PRIVATE)
@@ -18,9 +19,9 @@ async def chat_member_join_handler(chat_member: ChatMemberUpdated) -> None:
     await captcha.send_captcha(chat_member)
 
 
-@DP.callback_query(F.data.startswith("captcha:"))
-async def captcha_data_handler(callback_query: CallbackQuery) -> None:
-    await captcha.process_captcha_response(callback_query)
+@DP.callback_query(CaptchaCallback.filter())
+async def captcha_data_handler(callback_query: CallbackQuery, callback_data: CaptchaCallback) -> None:
+    await captcha.process_captcha_response(callback_query, callback_data)
 
 
 @DP.chat_member(ChatMemberUpdatedFilter(IS_MEMBER >> IS_NOT_MEMBER))
